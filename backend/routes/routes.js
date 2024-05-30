07f3496bb1feb14d0e4872a2fe1b56434a320ae3
Aarya R <aarya.masters@gmail.com>
2024-05-30 20:19:44 +0530

    small changes in routes n schema

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,6 +3,7 @@ const Week1 = require('../models/week1Model')
 const Week2 = require('../models/week2Model')
 const Week3 = require('../models/week3Model')
 const Week4 = require('../models/week4Model')
+const mongoose = require('mongoose')
 
 const router = express.Router()
 
@@ -10,6 +11,40 @@ router.get('/', (req,res) => {
     res.json({mssg:'Hi lol'})
 })
 
+//all get requests
+router.get('/week1', async (req, res) => {
+
+    const alltasks = await Week1.find({}).sort({ createdAt: -1 })
+
+
+    res.status(200).json(alltasks)
+})
+
+router.get('/week2', async (req, res) => {
+
+    const alltasks = await Week2.find({}).sort({ createdAt: -1 })
+
+
+    res.status(200).json(alltasks)
+})
+
+router.get('/week3', async (req, res) => {
+
+    const alltasks = await Week3.find({}).sort({ createdAt: -1 })
+
+
+    res.status(200).json(alltasks)
+})
+
+router.get('/week4', async (req, res) => {
+
+    const alltasks = await Week4.find({}).sort({ createdAt: -1 })
+
+
+    res.status(200).json(alltasks)
+})
+
+//all post requests
 router.post('/week1', async (req,res) => {
     const {name, phno, task1, task2, task3, task4, task5, task6} = req.body
 
@@ -52,6 +87,87 @@ router.post('/week4', async (req,res) => {
     }catch(error){
         res.status(400).json({error: error.message})
     }
+})
+
+//all patch requests
+router.patch('/week1/:id', async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such record for week1' })
+    }
+
+
+    const record = await Week1.findByIdAndUpdate({ _id: id }, {
+
+        ...req.body
+    })
+
+    if (!record) {
+        return res.status(404).json({ error: 'No such record for week1' })
+    }
+
+    res.status(200).json(record)
+})
+
+router.patch('/week2/:id', async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such record for week2' })
+    }
+
+
+    const record = await Week1.findByIdAndUpdate({ _id: id }, {
+
+        ...req.body
+    })
+
+    if (!record) {
+        return res.status(404).json({ error: 'No such record for week2' })
+    }
+
+    res.status(200).json(record)
+})
+
+router.patch('/week3/:id', async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such record for week3' })
+    }
+
+
+    const record = await Week1.findByIdAndUpdate({ _id: id }, {
+
+        ...req.body
+    })
+
+    if (!record) {
+        return res.status(404).json({ error: 'No such record for week3' })
+    }
+
+    res.status(200).json(record)
+})
+
+router.patch('/week4/:id', async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such record for week4' })
+    }
+
+
+    const record = await Week1.findByIdAndUpdate({ _id: id }, {
+
+        ...req.body
+    })
+
+    if (!record) {
+        return res.status(404).json({ error: 'No such record for week4' })
+    }
+
+    res.status(200).json(record)
 })
 
 module.exports = router
