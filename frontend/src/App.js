b1f5416d1aad93c55c77dc76191131409e21784d
roop1978 <juneja.roopanshi@gmail.com>
2024-06-02 22:57:19 +0530

    refresh error fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,41 @@
 // src/App.js
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Homepage from "./checkPages/Homepage";
 import Login from "./checkPages/Login";
 import Signup from "./checkPages/Signup";
-import Week1 from './checkPages/Week1';
-import Week2 from './checkPages/Week2';
-import Week3 from './checkPages/Week3';
-import Week4 from './checkPages/Week4';
-
+import Week1 from "./checkPages/Week1";
+import Week2 from "./checkPages/Week2";
+import Week3 from "./checkPages/Week3";
+import Week4 from "./checkPages/Week4";
+import ErrorPage from "./checkPages/ErrorPage";
 function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("user");
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
 
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Homepage user={user} setUser={setUser} />} />
-        <Route path="/login" element={<Login user={user} setUser={setUser} />} />
+        <Route
+          path="/login"
+          element={<Login user={user} setUser={setUser} />}
+        />
         <Route path="/signup" element={<Signup setUser={setUser} />} />
-        <Route path="/week1" element={<Week1 user={user}/>}></Route>
-        <Route path="/week2" element={<Week2 user={user}/>}></Route>
-        <Route path="/week3" element={<Week3 user={user}/>}></Route>
-        <Route path="/week4" element={<Week4 user={user}/>}></Route>
+        <Route path="/week1" element={<Week1 user={user} />}></Route>
+        <Route path="/week2" element={<Week2 user={user} />}></Route>
+        <Route path="/week3" element={<Week3 user={user} />}></Route>
+        <Route path="/week4" element={<Week4 user={user} />}></Route>
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/checkPages/ErrorPage.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+import { Link } from "react-router-dom";
+import niggesh from "../assets/niggesh1.jpg";
+const ErrorPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold text-red-600">404</h1>
+        <p className="text-2xl mt-4">
+          Oops! Looks like you foolishly stumbled upon a page that doesn't
+          exist.<br></br>
+          Smh 🤦‍♂️<br></br>
+          Niggesh here is working to fix it.
+          <img
+            src={niggesh}
+            style={{ width: "200px", height: "300px", display: "block", margin: "auto"}}
+            alt="niggesh"
+          ></img>
+          Meanwhile, you can go back home.
+        </p>
+        <Link to="/" className="btn btn-primary mt-6">
+          Go Back Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
