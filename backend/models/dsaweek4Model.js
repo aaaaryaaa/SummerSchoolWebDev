deac0588a69f7ef6a1ab06534aa480123cdb0041
Aarya R <aarya.masters@gmail.com>
2024-06-08 01:00:59 +0530

    all dbs n apis working in postman

---
+++ backend/models/dsaweek4Model.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+//creating schema 
+const dsaweek4 = new Schema({
+    _id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    task1: {
+        type: Boolean,
+        required: false
+    },
+    task2: {
+        type: Boolean,
+        required: false 
+    },
+    task3: {
+        type: Boolean,
+        required: false
+    },
+    task4: {
+        type: Boolean,
+        required: false
+    },
+    task5: {
+        type: Boolean,
+        required: false
+    },
+    task6: {
+        type: Boolean,
+        required: false
+    },
+    task7: {
+        type: Boolean,
+        required: false
+    },
+    task8: {
+        type: Boolean,
+        required: false
+    },
+    link1: {
+        type: String,
+        required: false
+    },
+    link2: {
+        type: String,
+        required: false 
+    }
+}, {timestamps: true})
+
+module.exports=mongoose.model('DSAWeek4', dsaweek4)
