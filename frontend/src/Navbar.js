a45a8889828ab13f60691fe0669e46dfe7d37657
Aarya R <aarya.masters@gmail.com>
2024-06-09 17:37:50 +0530

    nav fix

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -62,7 +62,7 @@ export default function Navbar({ user, setUser }) {
                                 </svg>
                             </button>
                         </div>
-                        <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
+                        <div className="items-center justify-between hidden w-full sm:flex md:w-auto md:order-1" id="navbar-cta">
                             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
                                 <li>
                                     <button onClick={handleGoHome} className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</button>
@@ -84,7 +84,7 @@ export default function Navbar({ user, setUser }) {
                                 </li>
                             </ul>
                         </div>
-                        <div className='lg:hidden w-full'>
+                        <div className='md:hidden lg:hidden w-full'>
                             {navOpen ? (
                                 <div className='w-full bg-transparent h-fit'>
                                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
