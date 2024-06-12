2c3541a31b7d8e2c205b8ce63281660fc9eeb6f3
roop1978 <juneja.roopanshi@gmail.com>
2024-06-12 19:45:07 +0530

    loading spinner added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-}
+export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [require("daisyui"), require("tailwindcss-filters")];
