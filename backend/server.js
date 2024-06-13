0ff927510f3a8af241b4527050102de32b157ca0
Aarya R <aarya.masters@gmail.com>
2024-06-13 11:02:31 +0530

    for deploy

--- backend/server.js
+++ backend/server.js
@@ -36,11 +36,10 @@ app.use((req, res, next) => {
 app.use("/api/progress", routes); // Mount the general routes
 app.use("/api/auth", authRoutes); // Mount the auth routes
 
-// app.use(express.static("../frontend/build"));
-// app.get("*", (req,res) => {
-//   // res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'))
-//   res.sendFile('../frontend/build/index.html', {root: __dirname});
-// })
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 const port = process.env.PORT || 3000;
 mongoose
